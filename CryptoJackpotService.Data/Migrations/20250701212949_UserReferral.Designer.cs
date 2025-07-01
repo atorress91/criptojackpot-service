5076c101f69af59218438a5939fe0be82b70fedc
AndresTS <andres91411@gmail.com>
2025-07-01 15:31:46 -0600

    Add migration for `UserReferral` table to manage user referral relationships, update related indexes in `users` table.

--- CryptoJackpotService.Data/Migrations/20250701212949_UserReferral.Designer.cs
+++ CryptoJackpotService.Data/Migrations/20250701212949_UserReferral.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using CryptoJackpotService.Data.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CryptoJackpotService.Data.Migrations
 {
     [DbContext(typeof(CryptoJackpotDbContext))]
-    partial class CryptoJackpotDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250701212949_UserReferral")]
+    partial class UserReferral
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CryptoJackpotService.Data/Migrations/20250701212949_UserReferral.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CryptoJackpotService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserReferral : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_users_identification",
+                table: "users");
+
+            migrationBuilder.DropIndex(
+                name: "ix_users_phone",
+                table: "users");
+
+            migrationBuilder.CreateTable(
+                name: "user_referral",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    referrer_id = table.Column<long>(type: "bigint", nullable: false),
+                    referred_id = table.Column<long>(type: "bigint", nullable: false),
+                    used_security_code = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    deleted_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_referral", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_user_referral_users_referred_id",
+                        column: x => x.referred_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "fk_user_referral_users_referrer_id",
+                        column: x => x.referrer_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_referral_referred_id",
+                table: "user_referral",
+                column: "referred_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_referral_referrer_id",
+                table: "user_referral",
+                column: "referrer_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_referral_used_security_code",
+                table: "user_referral",
+                column: "used_security_code");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user_referral");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_identification",
+                table: "users",
+                column: "identification",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_phone",
+                table: "users",
+                column: "phone",
+                unique: true);
+        }
+    }
+}
