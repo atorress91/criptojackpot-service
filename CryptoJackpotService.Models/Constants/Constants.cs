b574212f1f5e7c8ecebad137ec6c07d7e60f30d9
AndresTS <andres91411@gmail.com>
2025-07-10 13:05:18 -0600

    Switch to `ImmutableHashSet` for `AllowedExtensions` and add `System.Collections.Immutable` package dependency.

--- CryptoJackpotService.Models/Constants/Constants.cs
+++ CryptoJackpotService.Models/Constants/Constants.cs
@@ -1,12 +1,12 @@
-﻿namespace CryptoJackpotService.Models.Constants;
+﻿using System.Collections.Immutable;
+
+namespace CryptoJackpotService.Models.Constants;
 
 public static class Constants
 {
     public const string BaseUrl = "https://criptojackpot.com";
     public const string UrlConfirmEmail = "user_confirm_email";
     public const string ConfirmEmailTemplate = "email-confirmation.html";
-    public static readonly HashSet<string> AllowedExtensions = new(StringComparer.OrdinalIgnoreCase)
-    {
-        ".jpg", ".jpeg", ".png", ".webp"
-    };
+    public static readonly ImmutableHashSet<string> AllowedExtensions = 
+        ImmutableHashSet.Create(StringComparer.OrdinalIgnoreCase, ".jpg", ".jpeg", ".png", ".webp");
 }
